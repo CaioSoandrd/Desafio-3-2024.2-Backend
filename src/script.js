0908f1243bacbd068c0d328e40ec3a51fa53d374
Caio de Andrade <84874942+CaioSoandrd@users.noreply.github.com>
2025-02-26 20:48:24 -0300

    Fix Pokemon fetchPokemonList, consertei o tratamento de erroos

--- src/script.js
+++ src/script.js
@@ -11,21 +11,25 @@ function changeText(id, text) {
 // o código para resolver o desafio
 let pokemonList = [];
 let currentIndex = 0;
-try{
+
   async function fetchPokemonList() {
-
+try{
   const response =await fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1292');
-  const data = await response.json();
-  pokemonList= data.results;
 
-}
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+    const data = await response.json();
+  pokemonList= data.results;
+  await loadPokemon();
 }catch (err) {
   console.error('erro ao carregar a lista dos pokemons', err)
 
 }
-if (!response.ok) {
-  throw new Error(`HTTP error! status: ${response.status}`);
 }
+
+
+
 
 
 
@@ -33,10 +37,28 @@ function previousPokemon() {
   alert("Pokemon Anterior");
   //abra o terminal em inspecionar no chrome para visualizar
   console.log("Pokemon Anterior");
+  currentIndex = (currentIndex - 1) %pokemonList.length;
 }
 
 function nextPokemon() {
   alert("Pokemon Seguinte");
   //abra o terminal em inspecionar no chrome para visualizar
   console.log("Pokemon Seguinte");
+  currentIndex = (currentIndex +1 ) %pokemonList.length;
+}
+
+try{
+  async function loadPokemon(){
+const response = await fetch(pokemonList[currentIndex].url)
+    const pokemon = await response.json();
+
+const formName = pokemon.name[0].toUpperCase() +pokemon.name.slice(1);
+
+changeText('name', formName);
+changeImage('img_sprite_front_default', pokemon.sprites.front_default);
+
+  }
+
+}catch (e) {
+
 }
